f3da53e89ca5e46cf6e344ba45504eb98d77604e
abrauf6816 <armalik6816527@gmail.com>
2026-03-01 00:09:15 +0500

    Update orders.blade.php

--- resources/views/frontend/pages/orders.blade.php
+++ resources/views/frontend/pages/orders.blade.php
@@ -196,7 +196,7 @@
                             "X-CSRF-TOKEN": csrfToken,
                             "Content-Type": "application/json"
                         },
-                        credentials: "same-origin"
+                        credentials: "same-origin",
                         body: JSON.stringify({}) // include if backend expects JSON
                     })
                     .then(res => res.json())
