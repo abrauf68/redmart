0e06c7168d1b34eb3ae9f4afe16445bc053afc23
abrauf6816 <armalik6816527@gmail.com>
2026-04-26 01:56:49 +0500

    Update support-chat.blade.php

--- resources/views/frontend/pages/support-chat.blade.php
+++ resources/views/frontend/pages/support-chat.blade.php
@@ -1,18 +1,45 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Online Chat Support</title>
     <style>
-        html, body {
+        html,
+        body {
             margin: 0;
             height: 100%;
         }
+
         #crisp-container {
             width: 100%;
             height: 100%;
         }
+
+        .crisp-client .cc-1brb6 .cc-1xry0 {
+            width: 100% !important;
+            height: 100% !important;
+            max-height: 100% !important;
+            bottom: 0 !important;
+            right: 0 !important;
+            border-radius: 0 !important;
+        }
+
+        .crisp-client {
+            z-index: 9999 !important;
+        }
+
+        @media (max-width: 768px) {
+            .crisp-client .cc-1brb6 .cc-1xry0 {
+                width: 100% !important;
+                height: 100% !important;
+                bottom: 0 !important;
+                right: 0 !important;
+                border-radius: 0 !important;
+            }
+        }
     </style>
 </head>
+
 <body>
     <div id="crisp-container"></div>
     <!-- Crisp Chat Embedded -->
@@ -33,4 +60,5 @@
         })();
     </script>
 </body>
+
 </html>
